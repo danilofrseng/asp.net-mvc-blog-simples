95b5b449f47b67eb4b27590c55a2219939654dcc
Danilo <danilofrseng@gmail.com>
2021-07-14 13:13:32 -0300

    Início

---
+++ BlogPessoal.Web/Data/Contexto/BlogPessoalContexto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogPessoal.Web.Data.Contexto
+{
+    public class BlogPessoalContexto:System.Data.Entity.DbContext
+    {
+    }
+}
